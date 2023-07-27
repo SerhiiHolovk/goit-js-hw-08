acafe2e1a99c9b71f870f9f3118d18bb3de20104
SerhiiHolovk <98563452+SerhiiHolovk@users.noreply.github.com>
2023-07-27 13:12:19 +0300

    lkhgf

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const gallery = document.querySelector('.gallery')
+
+const galleryImages = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="Image ${description}" />
+   </a>
+ </li>`).join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryImages);
+
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+// import Player from '@vimeo/player';
+// console.log(Player);
+// const iframe = document.querySelector('iframe');
+// const player = new Player('vimeo-player');
+// //  // Отримуємо посилання на плеєр
+//         // var player = document.getElementById('vimeoPlayer');
+// player.on('timeUpdate', function() {
+//        var savedTime = localStorage.getItem('vimeoPlayerTime');
+// });
+// const onPlay = function(data) {
+//     // data is an object containing properties specific to that event
+// };
 
+// player.on('play', onPlay);
+
+// player.setCurrentTime(30.456).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
